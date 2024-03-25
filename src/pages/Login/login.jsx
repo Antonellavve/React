020e5edd2d62c5b3a9bab0a798d8d966b82b2016
antonellavve <antonellavvelazquez@gmail.com>
2024-03-25 19:53:54 -0300

    se corrigen modal

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import { Formik } from 'formik';
 import { Link } from 'react-router-dom';
 import { loginValidation } from '../../formik/Validation';
@@ -9,9 +10,11 @@ import { useDispatch } from 'react-redux';
 import { setCurrentUser } from '../../Redux/User/userSlice';
 import { Form, LoginEmail, LoginContainer } from "./LoginStyles";
 import { loginUser } from '../../axios/user';
+import Modal from 'react-modal';
 
 const Login = () => {
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
 
   useRedirect('/heroCheck');
 
@@ -31,6 +34,8 @@ const Login = () => {
               ...userData.user,
               token: userData.token
             }));
+          } else {
+            setShowModal(true); // Mostrar el modal si no se encuentra el correo
           }
         }}
       >
@@ -43,9 +48,35 @@ const Login = () => {
           </Link>
           <p>¿Olvidaste tu contraseña?</p>
           
-          <Submit >Ingresar</Submit>
+          <Submit>Ingresar</Submit>
         </Form>
       </Formik>
+
+      {/* Modal para mostrar cuando no se encuentra el correo */}
+      <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)}
+      style={{
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.803)',
+          zIndex: 1000,
+        },
+        content: {
+          width: '60%',
+          height: '10%',
+          margin: 'auto',
+          padding: '40px',
+          zIndex: 1001,
+          borderRadius: '10px',
+          color: 'black',
+          textAlign: 'center',
+        },
+      }}>
+        <h2>No se encontró el correo electrónico.</h2>
+        <Link to={"/register"} onClick={() => setShowModal(false)}
+        style={{color: 'black',
+        textAlign: 'center', 
+        textDecoration: 'underline', cursor: 'pointer' }}
+        >Registrarme</Link>
+      </Modal>
     </LoginContainer>
   );
 };
